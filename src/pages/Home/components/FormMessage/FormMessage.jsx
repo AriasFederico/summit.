b115a9449ae19ebf58f42423dada6a8a4815fcf7
Federico Arias <fede.ariass33@gmail.com>
2024-07-20 19:03:20 -0300

    modificando atributos

--- src/pages/Home/components/FormMessage/FormMessage.jsx
+++ src/pages/Home/components/FormMessage/FormMessage.jsx
@@ -2,7 +2,7 @@ import './FormMessage.css';
 
 export const FormMessage = () => {
   return (
-      <form name="contact" method="POST" netlify>
+      <form name="contact" netlify>
         <input type="hidden" name="form-name" value="contact" />
         <p>
           <label>Your Name: <input type="text" name="name" required /></label>
