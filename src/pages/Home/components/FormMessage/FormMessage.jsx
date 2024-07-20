50d6e85efd3b714afa25267936c6a3a64202feea
Federico Arias <fede.ariass33@gmail.com>
2024-07-20 19:34:23 -0300

    regresando el componente como estaba

--- src/pages/Home/components/FormMessage/FormMessage.jsx
+++ src/pages/Home/components/FormMessage/FormMessage.jsx
@@ -2,20 +2,6 @@ import './FormMessage.css';
 
 export const FormMessage = () => {
   return (
-      <form name="contact" method='POST' data-netlify='true' data-netlify-recaptcha="true">
-        <input type="hidden" name="name_of_my_form" value="name_of_my_form" />
-        <p>
-          <label>Your Name: <input type="text" name="name" required /></label>
-        </p>
-        <p>
-          <label>Your Email: <input type="email" name="email" required /></label>
-        </p>
-        <p>
-          <label>Message: <textarea name="message" required></textarea></label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
+    <div></div>
   );
 };
