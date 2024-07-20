643ac34364ae5028104b759dd5f7fd3c3f4f9cba
Federico Arias <fede.ariass33@gmail.com>
2024-07-20 19:57:55 -0300

    añadiendo api de form

--- src/pages/Home/components/FormMessage/FormMessage.jsx
+++ src/pages/Home/components/FormMessage/FormMessage.jsx
@@ -2,6 +2,19 @@ import './FormMessage.css';
 
 export const FormMessage = () => {
   return (
-    <div></div>
+    <div>
+      <form action="https://getform.io/f/adrydrqa"
+      method='POST'
+      encType='multipart/form-data'
+      >
+
+        <input type="text" name='name' />
+        <input type="email" name='email' />
+        <textarea name="message" id=""></textarea>
+
+        <button type='submit'>send</button>
+
+      </form>
+    </div>
   );
 };
