14549ec91bf182cb55c1d0292d3a1a9a062a07b2
Federico Arias <fede.ariass33@gmail.com>
2024-07-20 18:35:46 -0300

    modificando el formulario

--- src/pages/Home/components/FormMessage/FormMessage.jsx
+++ src/pages/Home/components/FormMessage/FormMessage.jsx
@@ -1,24 +1,27 @@
-import './FormMessage.css'
-import send from '../../../../assets/icons/send.svg'
+import './FormMessage.css';
+import send from '../../../../assets/icons/send.svg';
 
 export const FormMessage = () => {
-
   return (
     <div className="formmessage-container">
-      <form name="contact" method="POST" data-netlify="true">
-        <p>
-          <label>Your Name: <input type="text" name="name" /></label>
+      <form name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field">
+        <input type="hidden" name="form-name" value="contact" />
+        <p hidden>
+          <label>Don't fill this out: <input name="bot-field" /></label>
         </p>
         <p>
-          <label>Your Email: <input type="email" name="email" /></label>
+          <label>Your Name: <input type="text" name="name" required /></label>
         </p>
         <p>
-          <label>Message: <textarea name="message"></textarea></label>
+          <label>Your Email: <input type="email" name="email" required /></label>
+        </p>
+        <p>
+          <label>Message: <textarea name="message" required></textarea></label>
         </p>
         <p>
           <button type="submit">Send</button>
         </p>
       </form>
     </div>
-  )
-}
+  );
+};
